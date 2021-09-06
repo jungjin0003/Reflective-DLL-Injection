1848b42c652be55c1b160fb6b2f45c830d15a430
jungjin0003 <jungjin00031@gmail.com>
2021-09-06 13:05:56 +0900

    update Reflective.h

--- src/64bit/Reflective.h
+++ src/64bit/Reflective.h
@@ -2,3 +2,11 @@
 
 #include <stdio.h>
 #include <windows.h>
+
+#define MZ 0x5A4D
+#define PE 0x00004550
+
+typedef struct _BASE_RELOCATION_ENTRY {
+	USHORT Offset : 12;
+	USHORT Type : 4;
+} BASE_RELOCATION_ENTRY, *PBASE_RELOCATION_ENTRY;
